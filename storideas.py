fd9655b1077464e9d5c9aaba6a63714b8371a3cf
Januar_MLTVlogs <januarfmp@gmail.com>
2024-11-30 21:33:27 -0500

    Updating Storideas Source Code

--- storideas.py
+++ storideas.py
@@ -1,51 +1,54 @@
 import tkinter, tkinter.ttk as ttk
-from en import guiMenuEn
-from es import guiMenuEs
+from mod import en
+from mod import es
+from tkinter.font import Font
+
+#CONSTANTS
+TITLESIZE = 24
 
 #GO TO LANGUAGE MENU
 def gotolang(op,root):
     if(op == 1):
-        guiMenuEn(root)
+        en.guiMenu(root)
     if(op == 2):
-        guiMenuEs(root)
-    
-    
+        es.guiMenu(root)
 
 #MAIN GUI MENU
-def mainGuiMenu():
-    #Create the main window
-    window = tkinter.Tk()
-    window.resizable(False, False)
-    window.title("MLT Storideas")
+#Create the main window
+window = tkinter.Tk()
+window.resizable(False, False)
+window.title("MLT Storideas")
 
-    #Main Frame
-    mainframe = ttk.Frame(window)
-    mainframe.grid(padx=10, pady=10)
+#Defining Title Font
+titlefont = Font(size=TITLESIZE,
+                 weight="normal")
 
-    #Title
-    title = ttk.Label(mainframe, text="MLT STORIDEAS", font=("Arial", 16, "bold"))
-    title.pack()
+#Main Frame
+mainframe = ttk.Frame(window)
+mainframe.grid(padx=10, pady=10)
 
-    #Message
-    message = ttk.Label(mainframe, text="Select a language")
-    message.pack()
+#Title
+title = ttk.Label(mainframe, text="MLT STORIDEAS", font=titlefont)
+title.pack(padx=10,pady=10)
 
-    #Language Options
-    op = tkinter.IntVar()
+#Message
+message = ttk.Label(mainframe, text="Select a language")
+message.pack()
 
-    #English
-    eng = ttk.Radiobutton(mainframe, text="English", variable=op, value=1)
-    eng.pack()
+#Language Options
+op = tkinter.IntVar()
 
-    #Spanish
-    esp = ttk.Radiobutton(mainframe, text="Español", variable=op, value=2)
-    esp.pack()
+#English
+eng = ttk.Radiobutton(mainframe, text="English", variable=op, value=1)
+eng.pack()
 
-    #Button
-    button = ttk.Button(mainframe, text="Select", command=lambda: gotolang(op.get(),window))
-    button.pack()
+#Spanish
+esp = ttk.Radiobutton(mainframe, text="Español", variable=op, value=2)
+esp.pack()
+
+#Button
+button = ttk.Button(mainframe, text="Select", command=lambda: gotolang(op.get(),window))
+button.pack()
     
-    #Infinte loop
-    window.mainloop()
-
-mainGuiMenu()
+#Infinte loop
+window.mainloop()
